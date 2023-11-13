93dae2a72ed8730c350021d75367eaf6b09ad382
Carlos Ortega <the_king_00carlos@hotmail.com>
2023-11-13 02:58:29 -0600

    primer commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// Header stuff
+//  -  User control
+//  -  Title control
+// Form stuff
+//  -  Redirect to home
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
